3c96ea9288e1e020c7b43e5875505633c096c681
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-03-17 10:07:29 +0600

    for pull

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,6 +5,7 @@ const App = () => {
     <div
       style={{
         background: "linear-gradient(135deg, #0f0c29, #302b63, #24243e)",
+        height: "1200px",
       }}
     >
       <MainLayout />
