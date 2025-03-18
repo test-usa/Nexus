08748fc23f808d3cf5741f9dc42f56a66ce72586
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-03-18 14:26:21 +0600

    register form

--- src/components/Home/Shared/Register.tsx
+++ src/components/Home/Shared/Register.tsx
@@ -31,6 +31,7 @@ const Register = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1>Login Form</h1>
       <form
         onSubmit={handleSubmit}
         className="bg-white p-6 rounded-lg shadow-md w-96"
