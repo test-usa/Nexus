2294e6d6a2b21c189ab5a4474b3c34833fb0b881
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-03-18 14:58:02 +0600

    update some code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import CustomerReviews from "@/components/home/CustomerReviews";
 import EdgenuityBot from "@/components/home/EdgenuityBot";
 import FreeKeyCard from "@/components/home/FreeKeyCard";
 import { FrequentlyAsk } from "@/components/home/FrequentlyAsk";
-
 import Video from "@/components/home/Video";
 import Pricing from "@/components/Pricing";
 import CommonWrapper from "@/wrapper/CommonWrapper";
