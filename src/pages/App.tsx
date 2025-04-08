4123c991b7c2ff8a86f9ded0158258289bd8723f
bdCalling <bdcalling@bdCallings-Mac-mini-9.local>
2025-04-08 15:00:45 +0600

    update

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,7 @@ import MainLayout from "@/layout/MainLayout";
 
 const App = () => {
   return (
-    <div className="bg-[#181818]">
+    <div className=" bg-[#282D32]">
       <MainLayout />
     </div>
   );
